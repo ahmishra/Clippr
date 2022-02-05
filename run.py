93a0914ad113d5e90effa1d43fd3851cac13034c
ahmishra <umasaryan@gmail.com>
2022-02-05 11:54:02 +0530

    run.py fix

--- run.py
+++ run.py
@@ -6,4 +6,5 @@ PORT = 5000  # Port (Use any free port)
 
 if __name__ == "__main__":  # If this file is run directly, run the app
     db.create_all()  # Create the database, if it doesn't exist already
-    socketio.run(app, debug=DEBUG, host=HOST, port=PORT)  # Run the app (SOCKETIO)
+    # socketio.run(app, debug=DEBUG, host=HOST, port=PORT)  # Run the app (SOCKETIO)
+    socketio.run(app)
